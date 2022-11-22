ed3b434fe16fd6081ee045c773fcce8cc5b23e23
anurag_umarkhede <anuragumarkhede1995@gmail.com>
2022-11-22 17:29:02 +0530

    working code till add to fav for pet food and accessories

--- PetShopProject/PetShop/src/main/java/com/cybage/services/FavouriteListServiceImpl.java
+++ PetShopProject/PetShop/src/main/java/com/cybage/services/FavouriteListServiceImpl.java
@@ -2,7 +2,6 @@
 package com.cybage.services;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,33 +72,28 @@ public class FavouriteListServiceImpl implements IFavouriteListService {
 				favouriteItem.setPets(petList);
 				favouriteItemList.add(favouriteItem);
 			}
-//			else if(categoryName.equals("Cat"))
-//			{
-//				petList.add(petServiceImpl.findByPetId(id));
-//				favouriteItem.setPets(petList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//				
-//			}
-//			else if(categoryName.equals("Bird"))
-//			{
-//				petList.add(petServiceImpl.findByPetId(id));
-//				favouriteItem.setPets(petList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//				
-//			}
-//			else if(categoryName.equals("Fish"))
-//			{
-//				petList.add(petServiceImpl.findByPetId(id));
-//				favouriteItem.setPets(petList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//				
-//			}
+			else if(categoryName.equals("Cat"))
+			{
+				petList.add(petServiceImpl.findByPetId(id));
+				favouriteItem.setPets(petList);
+				favouriteItemList.add(favouriteItem);
+				
+			}
+			else if(categoryName.equals("Bird"))
+			{
+				petList.add(petServiceImpl.findByPetId(id));
+				favouriteItem.setPets(petList);
+				favouriteItemList.add(favouriteItem);
+				
+			}
+			else if(categoryName.equals("Fish"))
+			{
+				petList.add(petServiceImpl.findByPetId(id));
+				favouriteItem.setPets(petList);
+				favouriteItemList.add(favouriteItem);
+			}
 			pet.getFavouriteItem().addAll(favouriteItemList);
 		}
-		
 		
 		PetFood petFood = petFoodRepository.findByFoodId(id);
 		
@@ -109,83 +103,69 @@ public class FavouriteListServiceImpl implements IFavouriteListService {
 			
 			if(foodCategory.equals("Dog food"))
 			{
-				
 				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
-				System.out.println(petFoodServiceImpl.findByFoodId(id).getFoodName());
 				favouriteItem.setPetFoods(petFoodsList);
 				favouriteItemList.add(favouriteItem);
 			}
 			
+			else if(foodCategory.equals("Cat food"))
+			{
+				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
+				favouriteItem.setPetFoods(petFoodsList);
+				favouriteItemList.add(favouriteItem);
+			}
+			else if(foodCategory.equals("Bird food"))
+			{
+				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
+				favouriteItem.setPetFoods(petFoodsList);
+				favouriteItemList.add(favouriteItem);
+			}
+			else if(foodCategory.equals("Fish food"))
+			{
+				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
+				favouriteItem.setPetFoods(petFoodsList);
+				favouriteItemList.add(favouriteItem);
+			}
 			petFood.getFavouriteItem().addAll(favouriteItemList);
 		}
-//			else if(foodCategory.equals("Cat food"))
-//			{
-//				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
-//				favouriteItem.setPetFoods(petFoodsList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//			else if(foodCategory.equals("Bird food"))
-//			{
-//				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
-//				favouriteItem.setPetFoods(petFoodsList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//			else if(foodCategory.equals("Fish food"))
-//			{
-//				petFoodsList.add(petFoodServiceImpl.findByFoodId(id));
-//				favouriteItem.setPetFoods(petFoodsList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//		}
-//		
-//		
-//		PetAccessories petAccessories = petAccessoriesRepository.findByItemId(id);
-//		
-//		if(petAccessories != null)
-//		{
-//			String itemCategory = petAccessories.getItemCategory();
-//			
-//			if(itemCategory.equals("Dog accessories"))
-//			{
-//				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
-//				favouriteItem.setPetAccessories(petAccessoriesList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//			else if(itemCategory.equals("Cat accessories"))
-//			{
-//				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
-//				favouriteItem.setPetAccessories(petAccessoriesList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//			else if(itemCategory.equals("Bird accessories"))
-//			{
-//				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
-//				favouriteItem.setPetAccessories(petAccessoriesList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//			else if(itemCategory.equals("Fish accessories"))
-//			{
-//				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
-//				favouriteItem.setPetAccessories(petAccessoriesList);
-//				favouriteItemList.add(favouriteItem);
-//				favouriteList.setFavouriteItems(favouriteItemList);
-//			}
-//		}
-//		
-//		favouriteList.setSessionToken(sessionToken);
-//		System.out.println("fav item"+favouriteItem);
-//		System.out.println("fav list"+favouriteList);
 		
 		
-//		
+		PetAccessories petAccessories = petAccessoriesRepository.findByItemId(id);
+		
+		if(petAccessories != null)
+		{
+			String itemCategory = petAccessories.getItemCategory();
+			
+			if(itemCategory.equals("Dog accessories"))
+			{
+				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
+				favouriteItem.setPetAccessories(petAccessoriesList);
+				favouriteItemList.add(favouriteItem);
+			}
+			else if(itemCategory.equals("Cat accessories"))
+			{
+				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
+				favouriteItem.setPetAccessories(petAccessoriesList);
+				favouriteItemList.add(favouriteItem);
+			}
+			else if(itemCategory.equals("Bird accessories"))
+			{
+				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
+				favouriteItem.setPetAccessories(petAccessoriesList);
+				favouriteItemList.add(favouriteItem);
+			}
+			else if(itemCategory.equals("Fish accessories"))
+			{
+				petAccessoriesList.add(petAccessoriesServiceImpl.findByItemId(id));
+				favouriteItem.setPetAccessories(petAccessoriesList);
+				favouriteItemList.add(favouriteItem);
+			}
+			
+			petAccessories.getFavouriteItem().addAll(favouriteItemList);
+		}
+	
 		favouriteItem.setUserMail(user);
-//		
+		
 		return favouriteItemRepository.save(favouriteItem);
 	}
 
